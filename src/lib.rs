3fc2e0e8e696a40f1b07d686e1161177806ed7be
Matt Hunzinger <matthunz2@gmail.com>
2022-03-08 16:21:23 -0500

    Create Note and Pitch structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::{ops::Add, fmt::Debug};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Interval {
@@ -8,7 +8,7 @@ pub struct Interval {
 impl Interval {
     pub const MINOR_SECOND: Self = Self::new(1);
 
-    pub const WHOLE_STEP: Self = Self::new(2);
+    pub const MAJOR_SECOND: Self = Self::new(2);
 
     pub const MINOR_THIRD: Self = Self::new(3);
 
@@ -26,6 +26,53 @@ impl Add for Interval {
 
     fn add(self, rhs: Self) -> Self {
         Self::new(self.semitones() + rhs.semitones())
+    }
+}
+
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Pitch(u8);
+
+impl Pitch {
+    pub const C: Self = Self(0);
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Letter{
+    C
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Accidental {
+    Natrual,
+    Flat,
+    DoubleFlat,
+    Sharp,
+    DoubleSharp
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Note {
+    letter: Letter,
+    accidental: Accidental
+}
+
+impl Note {
+    pub const fn new(letter: Letter, accidental: Accidental) -> Self {
+        Self { letter, accidental }
+    } 
+    
+    pub const fn natural(letter: Letter) -> Self {
+        Self::new(letter, Accidental::Natrual)
+    }
+}
+
+impl From<Pitch> for Note {
+    fn from(note: Pitch) -> Self {
+        match note {
+            Pitch::C => Self::natural(Letter::C),
+            _ => todo!()
+        }
     }
 }
 
