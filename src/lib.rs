33de67915c8ac513d8d196e0a6160ddae14fcb9b
Matt Hunzinger <matthunz2@gmail.com>
2022-03-17 15:15:32 -0400

    Add basic transpose function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 mod interval;
 pub use interval::Interval;
+use note::pitch_note::PitchNote;
+use pitch::Pitch;
 
 pub mod note;
 
 pub mod pitch;
 
 pub mod scale;
+
+pub fn transpose(key: Pitch, note: Pitch, to: Pitch) -> Pitch {
+    let f = key - note;
+    to + f
+}
