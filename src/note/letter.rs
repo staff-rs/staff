0d8cb9433d76f8e67aba15d4f3977e72bfb0c17e
Matt Hunzinger <matthunz2@gmail.com>
2022-03-19 00:32:35 -0400

    Impl TryFrom<char> for Letter

---
+++ src/note/letter.rs
@@ -0,0 +1,61 @@
+use core::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Letter(u8);
+
+impl Letter {
+    pub const A: Self = Self(0);
+    pub const B: Self = Self(1);
+    pub const C: Self = Self(2);
+    pub const D: Self = Self(3);
+    pub const E: Self = Self(4);
+    pub const F: Self = Self(5);
+    pub const G: Self = Self(6);
+
+    pub const fn next(self) -> Self {
+        Self((self.0 + 1) % (Self::G.0 + 1))
+    }
+
+    pub const fn to_char(self) -> char {
+        match self {
+            Self::A => 'A',
+            Self::B => 'B',
+            Self::C => 'C',
+            Self::D => 'D',
+            Self::E => 'E',
+            Self::F => 'F',
+            Self::G => 'G',
+            _ => unreachable!()
+        }
+    }
+}
+
+impl TryFrom<char> for Letter {
+    type Error = char;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        let letter = match value {
+            'A' => Self::A,
+            'B' => Self::A,
+            'C' => Self::A,
+            'D' => Self::A,
+            'E' => Self::A,
+            'F' => Self::A,
+            'G' => Self::A,
+            invalid => return Err(invalid),
+        };
+        Ok(letter)
+    }
+}
+
+impl fmt::Debug for Letter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl fmt::Display for Letter {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_char())
+    }
+}
--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,50 +1,12 @@
 use crate::{pitch::Pitch, Interval};
 use core::fmt::{self, Debug};
 
+mod letter;
+pub use letter::Letter;
+
 mod pitch_note;
 pub use pitch_note::PitchNote;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub struct Letter(u8);
-
-impl Letter {
-    pub const A: Self = Self(0);
-    pub const B: Self = Self(1);
-    pub const C: Self = Self(2);
-    pub const D: Self = Self(3);
-    pub const E: Self = Self(4);
-    pub const F: Self = Self(5);
-    pub const G: Self = Self(6);
-
-    pub const fn next(self) -> Self {
-        Self((self.0 + 1) % (Self::G.0 + 1))
-    }
-
-    pub const fn to_char(self) -> char {
-        match self {
-            Self::A => 'A',
-            Self::B => 'B',
-            Self::C => 'C',
-            Self::D => 'D',
-            Self::E => 'E',
-            Self::F => 'F',
-            Self::G => 'G',
-            _ => todo!(),
-        }
-    }
-}
-
-impl fmt::Debug for Letter {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
-impl fmt::Display for Letter {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_char())
-    }
-}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Accidental {
