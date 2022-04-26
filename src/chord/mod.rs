4ed0f87d65b4ae91a41dfcba1820ae2757c585af
Matt Hunzinger <matthunz2@gmail.com>
2022-04-26 00:38:53 -0400

    Handle Chord bass notes without inversions

--- src/chord/mod.rs
+++ src/chord/mod.rs
@@ -3,24 +3,39 @@ use crate::{Interval, Pitch, Set};
 mod kind;
 pub use kind::ChordKind;
 
+pub struct Inversions {
+    chord: Chord,
+    pitches: Set<Pitch>,
+}
+
+impl Iterator for Inversions {
+    type Item = Chord;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pitches.next().map(|pitch| self.chord.inversion(pitch))
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct Chord {
     pub root: Pitch,
     pub bass: Option<Pitch>,
+    pub is_inversion: bool,
     pub pitches: Set<Pitch>,
 }
 
 impl Chord {
-    pub fn new(root: Pitch, bass: Option<Pitch>, pitches: Set<Pitch>) -> Self {
+    pub fn new(root: Pitch, bass: Option<Pitch>, is_inversion: bool, pitches: Set<Pitch>) -> Self {
         Self {
             root,
             bass,
+            is_inversion,
             pitches,
         }
     }
 
     pub fn from_root(root: Pitch) -> Self {
-        Self::new(root, None, Set::default())
+        Self::new(root, None, false, Set::default())
     }
 
     pub fn triad(root: Pitch, third: Interval, fifth: Interval) -> Self {
@@ -29,6 +44,15 @@ impl Chord {
         me
     }
 
+    /// ```
+    /// use music_note::{Chord, Pitch};
+    ///
+    /// // C Major
+    /// let chord = Chord::major(Pitch::C);
+    ///
+    /// let notes = [Pitch::C, Pitch::E, Pitch::G];
+    /// assert!(chord.into_iter().eq(notes));
+    /// ```
     pub fn major(root: Pitch) -> Self {
         Self::triad(root, Interval::MAJOR_THIRD, Interval::PERFECT_FIFTH)
     }
@@ -45,9 +69,23 @@ impl Chord {
     /// use music_note::{Chord, Pitch};
     ///
     /// // C Major (1st inversion)
-    /// let chord = Chord::major(Pitch::C).with_bass(Pitch::E);
+    /// let chord = Chord::major(Pitch::C).inversion(Pitch::E);
     ///
     /// let notes = [Pitch::E, Pitch::G, Pitch::C];
+    /// assert!(chord.into_iter().eq(notes));
+    /// ```
+    pub fn inversion(mut self, pitch: Pitch) -> Self {
+        self.is_inversion = true;
+        self.with_bass(pitch)
+    }
+
+    /// ```
+    /// use music_note::{Chord, Pitch};
+    ///
+    /// // C/B
+    /// let chord = Chord::major(Pitch::C).with_bass(Pitch::B);
+    ///
+    /// let notes = [Pitch::B, Pitch::C, Pitch::E, Pitch::G];
     /// assert!(chord.into_iter().eq(notes));
     /// ```
     pub fn with_bass(mut self, pitch: Pitch) -> Self {
@@ -71,6 +109,13 @@ impl Chord {
     pub fn is_rootless(self) -> bool {
         !self.pitches.contains(self.root)
     }
+
+    pub fn inversions(self) -> Inversions {
+        Inversions {
+            chord: self,
+            pitches: self.pitches,
+        }
+    }
 }
 
 impl Extend<Pitch> for Chord {
@@ -93,7 +138,11 @@ impl IntoIterator for Chord {
 
     fn into_iter(self) -> Self::IntoIter {
         let (high, low) = if let Some(bass) = self.bass {
-            self.pitches.split(bass)
+            if self.is_inversion {
+                self.pitches.split(bass)
+            } else {
+                (self.pitches, [bass].into_iter().collect())
+            }
         } else {
             (Set::default(), self.pitches)
         };
@@ -121,9 +170,5 @@ mod tests {
     use crate::Pitch;
 
     #[test]
-    fn f() {
-        let c = Chord::major(Pitch::C).with_bass(Pitch::E);
-        let notes: Vec<_> = c.into_iter().collect();
-        dbg!(notes);
-    }
+    fn f() {}
 }
