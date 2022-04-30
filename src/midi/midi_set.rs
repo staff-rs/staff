2d85b1f704f9c3248d1dadf57cdace7e16251f76
Matt Hunzinger <matthunz2@gmail.com>
2022-04-30 00:56:08 -0400

    Impl Extend and FromIterator for MidiSet

--- src/midi/midi_set.rs
+++ src/midi/midi_set.rs
@@ -8,12 +8,50 @@ pub struct MidiSet {
 }
 
 impl MidiSet {
+    pub fn contains(&mut self, midi: MidiNote) -> bool {
+        with_midi(self.low, self.high, midi, |set, midi| set.contains(midi))
+    }
+
     pub fn push(&mut self, midi: MidiNote) {
-        if midi > MidiNote::from_byte(63) {
-            let byte = midi.into_byte() - 63;
-            self.high.push(MidiNote::from(byte))
-        } else {
-            self.low.push(midi)
+        self.inner(midi, |set, midi| set.push(midi))
+    }
+
+    pub fn remove(&mut self, midi: MidiNote) {
+        self.inner(midi, |set, midi| set.remove(midi))
+    }
+
+    fn inner<F, T>(&mut self, midi: MidiNote, f: F) -> T
+    where
+        F: FnOnce(&mut Set<MidiNote, u64>, MidiNote) -> T,
+    {
+        with_midi(&mut self.low, &mut self.high, midi, f)
+    }
+}
+
+fn with_midi<T, U, F>(low: T, high: T, midi: MidiNote, f: F) -> U
+where
+    F: FnOnce(T, MidiNote) -> U,
+{
+    if midi <= MidiNote::from_byte(63) {
+        f(low, midi)
+    } else {
+        let byte = midi.into_byte() - 63;
+        f(high, MidiNote::from(byte))
+    }
+}
+
+impl FromIterator<MidiNote> for MidiSet {
+    fn from_iter<T: IntoIterator<Item = MidiNote>>(iter: T) -> Self {
+        let mut set = Self::default();
+        set.extend(iter);
+        set
+    }
+}
+
+impl Extend<MidiNote> for MidiSet {
+    fn extend<T: IntoIterator<Item = MidiNote>>(&mut self, iter: T) {
+        for midi in iter {
+            self.push(midi);
         }
     }
 }
