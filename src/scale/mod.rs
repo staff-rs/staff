5f663a4efe55c3ddabb540976cde9726cce82d4b
Matt Hunzinger <matthunz2@gmail.com>
2022-05-04 19:03:49 -0400

    Add common scale constructors

--- src/scale/mod.rs
+++ src/scale/mod.rs
@@ -5,10 +5,109 @@ use core::ops::Add;
 mod degree;
 pub use degree::Degree;
 
+pub type DiatonicScale<T> = Scale<T, Diatonic<T, Set<Interval, u16>>>;
+
+impl<T> DiatonicScale<T>
+where
+    T: Degree + Clone,
+{
+    pub fn major(root: T) -> Self {
+        Self::from_array(
+            root,
+            [
+                Interval::UNISON,
+                Interval::MAJOR_SECOND,
+                Interval::MAJOR_THIRD,
+                Interval::PERFECT_FOURTH,
+                Interval::PERFECT_FIFTH,
+                Interval::MAJOR_SIXTH,
+                Interval::MAJOR_SEVENTH,
+            ],
+        )
+    }
+
+    pub fn natural_minor(root: T) -> Self {
+        Self::from_array(
+            root,
+            [
+                Interval::UNISON,
+                Interval::MAJOR_SECOND,
+                Interval::MINOR_THIRD,
+                Interval::PERFECT_FOURTH,
+                Interval::PERFECT_FIFTH,
+                Interval::MINOR_SIXTH,
+                Interval::MINOR_SEVENTH,
+            ],
+        )
+    }
+
+    /// ```
+    /// use music_note::{Natural, Note, Scale};
+    ///
+    /// let scale = Scale::harmonic_minor(Note::from(Natural::A));
+    ///
+    /// assert!(scale.eq([
+    ///     Note::from(Natural::A),
+    ///     Note::from(Natural::B),
+    ///     Note::from(Natural::C),
+    ///     Note::from(Natural::D),
+    ///     Note::from(Natural::E),
+    ///     Note::from(Natural::F),
+    ///     Note::sharp(Natural::G),
+    /// ]));
+    /// ```
+    pub fn harmonic_minor(root: T) -> Self {
+        Self::from_array(
+            root,
+            [
+                Interval::UNISON,
+                Interval::MAJOR_SECOND,
+                Interval::MINOR_THIRD,
+                Interval::PERFECT_FOURTH,
+                Interval::PERFECT_FIFTH,
+                Interval::MINOR_SIXTH,
+                Interval::MAJOR_SEVENTH,
+            ],
+        )
+    }
+
+    pub fn melodic_minor(root: T) -> Self {
+        Self::from_array(
+            root,
+            [
+                Interval::UNISON,
+                Interval::MAJOR_SECOND,
+                Interval::MINOR_THIRD,
+                Interval::PERFECT_FOURTH,
+                Interval::PERFECT_FIFTH,
+                Interval::MAJOR_SIXTH,
+                Interval::MAJOR_SEVENTH,
+            ],
+        )
+    }
+
+    fn from_array<const N: usize>(root: T, array: [Interval; N]) -> Self {
+        Self::diatonic(root, array.into_iter().collect())
+    }
+}
+
 pub struct Diatonic<T: Degree, U> {
     degree: T,
     state: T::State,
     intervals: U,
+}
+
+impl<T, U> Diatonic<T, U>
+where
+    T: Degree + Clone,
+{
+    pub fn new(root: T, intervals: U) -> Self {
+        Self {
+            degree: root.clone(),
+            state: root.state(),
+            intervals,
+        }
+    }
 }
 
 pub struct Scale<T, U> {
@@ -16,29 +115,36 @@ pub struct Scale<T, U> {
     intervals: U,
 }
 
-impl<T> Scale<T, Diatonic<T, Set<Interval, u16>>>
+impl<T, U> Scale<T, U> {
+    pub fn new(root: T, intervals: U) -> Self {
+        Self { root, intervals }
+    }
+}
+
+impl<T, U> Scale<T, Diatonic<T, U>>
 where
     T: Degree + Clone,
 {
-    pub fn major(root: T) -> Self {
-        Self {
-            root: root.clone(),
-            intervals: Diatonic {
-                degree: root.clone(),
-                state: root.state(),
-                intervals: [
-                    Interval::UNISON,
-                    Interval::MAJOR_SECOND,
-                    Interval::MAJOR_THIRD,
-                    Interval::PERFECT_FOURTH,
-                    Interval::PERFECT_FIFTH,
-                    Interval::MAJOR_SIXTH,
-                    Interval::MAJOR_SEVENTH,
-                ]
-                .into_iter()
-                .collect(),
-            },
-        }
+    pub fn diatonic(root: T, intervals: U) -> Self {
+        Self::new(root.clone(), Diatonic::new(root, intervals))
+    }
+}
+
+impl<T> Scale<T, Set<Interval, u16>> {
+    pub fn blues(root: T) -> Self {
+        Self::new(
+            root,
+            [
+                Interval::UNISON,
+                Interval::MINOR_THIRD,
+                Interval::PERFECT_FOURTH,
+                Interval::TRITONE,
+                Interval::PERFECT_FIFTH,
+                Interval::MINOR_SEVENTH,
+            ]
+            .into_iter()
+            .collect(),
+        )
     }
 }
 
