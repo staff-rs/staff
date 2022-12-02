cf5d9d52cfbc5373eb34d40e042be25ee3b67bc2
Matt Hunzinger <matthunz2@gmail.com>
2022-12-02 18:30:45 -0500

    Render music glyphs

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,5 @@
 use svg::{
-    node::element::{Ellipse, Line, Path},
+    node::element::{Ellipse, Line, Path, Rectangle},
     Document, Node,
 };
 
@@ -25,130 +25,167 @@ const NOTE_RY: i64 = 6;
 
 impl Measure {
     pub fn write_svg(&self, doc: &mut Document, x: i64) {
-        doc.append(Path::new().set("transform", "translate(0, -11)").set("d", include_str!("../svg/treble_clef_d")));
+        doc.append(
+            Path::new()
+                .set("transform", "translate(0, -11)")
+                .set("d", include_str!("../svg/treble_clef_d")),
+        );
 
         let mut chord_x = x + 68;
-
         let mut pos = 0;
         while let Some(chord) = self.chords.get(pos) {
-            chord_x += match chord.duration {
-                Duration::Whole => {
-                    chord.write_svg(doc, chord_x);
-                    200
+            chord_x += if chord.notes.is_empty() {
+                match chord.duration {
+                    Duration::Whole => doc.append(
+                        Rectangle::new()
+                            .set("width", 20)
+                            .set("height", 6)
+                            .set("x", chord_x)
+                            .set("y", 62),
+                    ),
+                    Duration::Half => doc.append(
+                        Rectangle::new()
+                            .set("width", 20)
+                            .set("height", 6)
+                            .set("x", chord_x)
+                            .set("y", 68),
+                    ),
+                    Duration::Quarter => {
+                        doc.append(
+                            Path::new()
+                                .set("transform", format!("translate({chord_x}, 64)"))
+                                .set("d", include_str!("../svg/quarter_rest.txt")),
+                        );
+                    }
+                    Duration::Eigth => {
+                        doc.append(
+                            Path::new()
+                                .set("transform", format!("translate({chord_x}, 64)"))
+                                .set("d", include_str!("../svg/eigth_rest.txt")),
+                        );
+                    }
                 }
-                Duration::Half => {
-                    chord.write_svg(doc, chord_x);
-                    chord.draw_note_line(doc, chord_x, 40);
-                    200 / 2
-                }
-                Duration::Quarter => {
-                    chord.write_svg(doc, chord_x);
-                    chord.draw_note_line(doc, chord_x, 40);
-                    200 / 4
-                }
-                Duration::Eigth => {
-                    chord.write_svg(doc, chord_x);
 
-                    let low = *chord.notes.iter().min().unwrap();
-                    let high = *chord.notes.iter().max().unwrap();
-                    let diff = low.max(10 - high);
+                49
+            } else {
+                match chord.duration {
+                    Duration::Whole => {
+                        chord.write_svg(doc, chord_x);
+                        200
+                    }
+                    Duration::Half => {
+                        chord.write_svg(doc, chord_x);
+                        chord.draw_note_line(doc, chord_x + NOTE_RX, 40);
+                        200 / 2
+                    }
+                    Duration::Quarter => {
+                        chord.write_svg(doc, chord_x);
+                        chord.draw_note_line(doc, chord_x + NOTE_RX, 40);
+                        200 / 4
+                    }
+                    Duration::Eigth => {
+                        chord.write_svg(doc, chord_x);
 
-                    if let Some(next) = self.chords.get(pos + 1) {
-                        if next.duration == Duration::Eigth {
-                            let low_next = *next.notes.iter().min().unwrap();
-                            let high_next = *next.notes.iter().max().unwrap();
-                            let diff_next = low_next.max(10 - high_next);
+                        let low = *chord.notes.iter().min().unwrap();
+                        let high = *chord.notes.iter().max().unwrap();
+                        let diff = low.max(10 - high);
 
-                            let (x, y1, y2, y1_next, y2_next, y) = if diff > diff_next {
-                                if low > 10 - high {
-                                    (
-                                        chord_x,
-                                        note_y(low) + 40,
-                                        note_y(high),
-                                        note_y(low) + 40,
-                                        note_y(high_next),
-                                        note_y(low) + 40,
-                                    )
+                        if let Some(next) = self.chords.get(pos + 1) {
+                            if next.duration == Duration::Eigth {
+                                let low_next = *next.notes.iter().min().unwrap();
+                                let high_next = *next.notes.iter().max().unwrap();
+                                let diff_next = low_next.max(10 - high_next);
+
+                                let (x, y1, y2, y1_next, y2_next, y) = if diff > diff_next {
+                                    if low > 10 - high {
+                                        (
+                                            chord_x,
+                                            note_y(low) + 40,
+                                            note_y(high),
+                                            note_y(low) + 40,
+                                            note_y(high_next),
+                                            note_y(low) + 40,
+                                        )
+                                    } else {
+                                        (
+                                            chord_x + 8,
+                                            note_y(low),
+                                            note_y(high_next) - 40,
+                                            note_y(low_next),
+                                            note_y(high_next) - 40,
+                                            note_y(high_next) - 40 + 4,
+                                        )
+                                    }
                                 } else {
-                                    (
-                                        chord_x + 8,
-                                        note_y(low),
-                                        note_y(high_next) - 40,
-                                        note_y(low_next),
-                                        note_y(high_next) - 40,
-                                        note_y(high_next) - 40 + 4,
-                                    )
-                                }
+                                    if low_next > 10 - high_next {
+                                        (
+                                            chord_x,
+                                            note_y(low) + 40,
+                                            note_y(high_next),
+                                            note_y(low_next) + 40,
+                                            note_y(high_next),
+                                            note_y(high_next),
+                                        )
+                                    } else {
+                                        (
+                                            chord_x - 8,
+                                            note_y(high),
+                                            note_y(low_next) + 40,
+                                            note_y(high_next),
+                                            note_y(low_next) + 40,
+                                            note_y(low_next) + 40 - 4,
+                                        )
+                                    }
+                                };
+
+                                doc.append(
+                                    Line::new()
+                                        .set("fill", "none")
+                                        .set("stroke", "black")
+                                        .set("stroke-width", 2)
+                                        .set("x1", x - 1)
+                                        .set("y1", y1)
+                                        .set("x2", x - 1)
+                                        .set("y2", y2),
+                                );
+
+                                const OFFSET: i64 = 30;
+                                let next_x = x + OFFSET;
+                                doc.append(
+                                    Line::new()
+                                        .set("fill", "none")
+                                        .set("stroke", "black")
+                                        .set("stroke-width", 2)
+                                        .set("x1", next_x - 1)
+                                        .set("y1", y1_next)
+                                        .set("x2", next_x - 1)
+                                        .set("y2", y2_next),
+                                );
+
+                                doc.append(
+                                    Line::new()
+                                        .set("fill", "none")
+                                        .set("stroke", "black")
+                                        .set("stroke-width", 8)
+                                        .set("x1", x)
+                                        .set("y1", y)
+                                        .set("x2", next_x)
+                                        .set("y2", y),
+                                );
+
+                                next.write_svg(doc, chord_x + OFFSET);
+
+                                pos += 1;
+                                70
                             } else {
-                                if low_next > 10 - high_next {
-                                    (
-                                        chord_x,
-                                        note_y(low) + 40,
-                                        note_y(high_next),
-                                        note_y(low_next) + 40,
-                                        note_y(high_next),
-                                        note_y(high_next),
-                                    )
-                                } else {
-                                    (
-                                        chord_x - 8,
-                                        note_y(high),
-                                        note_y(low_next) + 40,
-                                        note_y(high_next),
-                                        note_y(low_next) + 40,
-                                        note_y(low_next) + 40 - 4,
-                                    )
-                                }
-                            };
-
-                            doc.append(
-                                Line::new()
-                                    .set("fill", "none")
-                                    .set("stroke", "black")
-                                    .set("stroke-width", 2)
-                                    .set("x1", x)
-                                    .set("y1", y1)
-                                    .set("x2", x)
-                                    .set("y2", y2),
-                            );
-
-                            const OFFSET: i64 = 30;
-                            let next_x = x + OFFSET;
-                            doc.append(
-                                Line::new()
-                                    .set("fill", "none")
-                                    .set("stroke", "black")
-                                    .set("stroke-width", 2)
-                                    .set("x1", next_x)
-                                    .set("y1", y1_next)
-                                    .set("x2", next_x)
-                                    .set("y2", y2_next),
-                            );
-
-                            doc.append(
-                                Line::new()
-                                    .set("fill", "none")
-                                    .set("stroke", "black")
-                                    .set("stroke-width", 8)
-                                    .set("x1", x)
-                                    .set("y1", y)
-                                    .set("x2", next_x)
-                                    .set("y2", y),
-                            );
-
-                            next.write_svg(doc, chord_x + OFFSET);
-
-                            pos += 1;
-                            70
+                                20
+                            }
                         } else {
                             20
                         }
-                    } else {
-                        20
                     }
                 }
             };
-
             pos += 1;
         }
 
@@ -185,6 +222,12 @@ pub struct Chord {
     // TODO use Set
     notes: Vec<i64>,
     duration: Duration,
+}
+
+impl Chord {
+    pub fn new(notes: Vec<i64>, duration: Duration) -> Self {
+        Self { notes, duration }
+    }
 }
 
 fn note_y(note: i64) -> i64 {
@@ -257,9 +300,10 @@ impl Chord {
                 Line::new()
                     .set("fill", "none")
                     .set("stroke", "black")
-                    .set("x1", x)
+                    .set("stroke-width", 2)
+                    .set("x1", x - 1)
                     .set("y1", note_y(low) + extra)
-                    .set("x2", x)
+                    .set("x2", x - 1)
                     .set("y2", note_y(high)),
             );
             note_y(low) + 40
@@ -268,9 +312,10 @@ impl Chord {
                 Line::new()
                     .set("fill", "none")
                     .set("stroke", "black")
-                    .set("x1", x + NOTE_RX)
+                    .set("stroke-width", 2)
+                    .set("x1", x + NOTE_RX - 1)
                     .set("y1", note_y(low))
-                    .set("x2", x + NOTE_RX)
+                    .set("x2", x + NOTE_RX - 1)
                     .set("y2", note_y(high) - extra),
             );
             note_y(high) - 40
@@ -286,22 +331,7 @@ mod tests {
     fn f() {
         let measure = Measure {
             chords: vec![
-                Chord {
-                    notes: vec![5, 7, 9],
-                    duration: Duration::Eigth,
-                },
-                Chord {
-                    notes: vec![5],
-                    duration: Duration::Eigth,
-                },
-                Chord {
-                    notes: vec![-2],
-                    duration: Duration::Eigth,
-                },
-                Chord {
-                    notes: vec![-2, 0, 2],
-                    duration: Duration::Eigth,
-                },
+                Chord::new(vec![], Duration::Half),
                 Chord {
                     notes: vec![-3],
                     duration: Duration::Eigth,
@@ -311,12 +341,8 @@ mod tests {
                     duration: Duration::Eigth,
                 },
                 Chord {
-                    notes: vec![4],
-                    duration: Duration::Eigth,
-                },
-                Chord {
-                    notes: vec![5, 4, 3],
-                    duration: Duration::Eigth,
+                    notes: vec![7],
+                    duration: Duration::Quarter,
                 },
             ],
         };
