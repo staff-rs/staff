d1ac7337ed3789067e50a2d1a06d25f3cf0195fc
Matt Hunzinger <matthunz2@gmail.com>
2022-12-03 22:31:55 -0500

    Render staggered notes

--- src/score.rs
+++ src/score.rs
@@ -3,32 +3,88 @@ use svg::{
     Node,
 };
 
+use crate::note;
+
+pub struct Note {
+    index: i64,
+    x: f64,
+}
+
+pub struct Chord {
+    width: f64,
+    notes: Vec<Note>,
+}
+
+impl Chord {
+    pub fn new(notes: &[i64], renderer: &Renderer) -> Self {
+        let low = *notes.iter().min().unwrap();
+        let high = *notes.iter().max().unwrap();
+
+        let is_upside_down = low + 10 > high;
+
+        let mut is_staggered = false;
+        let notes = notes
+            .iter()
+            .copied()
+            .map(|index| {
+                let x = if index & 1 == 0 && notes.contains(&(index - 1)) {
+                    is_staggered = true;
+                    if is_upside_down {
+                        renderer.note_rx
+                    } else {
+                        0.
+                    }
+                } else {
+                    if is_upside_down {
+                        0.
+                    } else {
+                        renderer.note_rx
+                    }
+                };
+                Note { index, x }
+            })
+            .collect();
+
+        let width = if is_staggered {
+            renderer.note_rx * 2.
+        } else {
+            renderer.note_rx * 4.
+        };
+
+        Self { width, notes }
+    }
+
+    pub fn svg<T: Node>(&self, renderer: &Renderer, node: &mut T) {
+        for note in &self.notes {
+            node.append(
+                Ellipse::new()
+                    .set("cx", note.x + renderer.note_rx / 2.)
+                    .set("cy", renderer.note_ry * note.index as f64)
+                    .set("rx", renderer.note_ry)
+                    .set("ry", renderer.note_ry),
+            )
+        }
+    }
+}
+
 pub struct Renderer {
     pub note_rx: f64,
     pub note_ry: f64,
 }
 
 impl Renderer {
-    pub fn render_chord<T: Node>(&self, node: &mut T, notes: &[i64]) {
-        for note in notes {
-            node.append(
-                Ellipse::new()
-                    .set("cx", 50)
-                    .set("cy", self.note_ry * *note as f64)
-                    .set("rx", self.note_ry)
-                    .set("ry", self.note_ry),
-            )
-        }
+    pub fn svg<T: Node>(&self, node: &mut T, chord: Chord) {
+       chord.svg(self, node);
 
         for line in 0..5 {
             node.append(
                 Line::new()
-                .set("stroke", "#000")
-                .set("stroke-width", 2)
+                    .set("stroke", "#000")
+                    .set("stroke-width", 2)
                     .set("x1", 0)
-                    .set("y1", line as f64 * self.note_ry)
-                    .set("x2", 100)
-                    .set("y2", line as f64 * self.note_ry),
+                    .set("y1", (line * 2) as f64 * self.note_ry)
+                    .set("x2", chord.width)
+                    .set("y2", (line * 2) as f64 * self.note_ry),
             )
         }
     }
@@ -36,7 +92,7 @@ impl Renderer {
 
 #[cfg(test)]
 mod tests {
-    use super::Renderer;
+    use super::{Chord, Renderer};
 
     #[test]
     fn f() {
@@ -46,7 +102,7 @@ mod tests {
             note_rx: 10.,
             note_ry: 6.,
         };
-        renderer.render_chord(&mut document, &[1, 2, 3]);
+        renderer.svg(&mut document, Chord::new(&[5, 6, 7], &renderer));
         svg::save("image.svg", &document).unwrap();
     }
 }
