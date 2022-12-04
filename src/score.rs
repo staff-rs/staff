003fc1cb9f1196aba6c353c4a09ccbbe89245c10
Matt Hunzinger <matthunz2@gmail.com>
2022-12-03 23:55:28 -0500

    Render with correct spacing

--- src/score.rs
+++ src/score.rs
@@ -3,8 +3,6 @@ use svg::{
     Node,
 };
 
-use crate::note;
-
 pub struct Note {
     index: i64,
     x: f64,
@@ -54,11 +52,11 @@ impl Chord {
         Self { width, notes }
     }
 
-    pub fn svg<T: Node>(&self, renderer: &Renderer, node: &mut T) {
+    pub fn svg<T: Node>(&self, renderer: &Renderer, node: &mut T, x: f64) {
         for note in &self.notes {
             node.append(
                 Ellipse::new()
-                    .set("cx", note.x + renderer.note_rx / 2.)
+                    .set("cx", x + note.x + renderer.note_rx / 2.)
                     .set("cy", renderer.note_ry * note.index as f64)
                     .set("rx", renderer.note_ry)
                     .set("ry", renderer.note_ry),
@@ -70,23 +68,42 @@ impl Chord {
 pub struct Renderer {
     pub note_rx: f64,
     pub note_ry: f64,
+    pub padding: f64,
+    pub stroke_width: f64,
 }
 
 impl Renderer {
     pub fn svg<T: Node>(&self, node: &mut T, chord: Chord) {
-       chord.svg(self, node);
+        let x = self.stroke_width + self.padding;
+        chord.svg(self, node, x);
 
         for line in 0..5 {
-            node.append(
-                Line::new()
-                    .set("stroke", "#000")
-                    .set("stroke-width", 2)
-                    .set("x1", 0)
-                    .set("y1", (line * 2) as f64 * self.note_ry)
-                    .set("x2", chord.width)
-                    .set("y2", (line * 2) as f64 * self.note_ry),
-            )
+            self.draw_line(
+                node,
+                self.stroke_width / 2.,
+                (line * 2) as f64 * self.note_ry,
+                self.stroke_width + chord.width + self.padding * 2.,
+                (line * 2) as f64 * self.note_ry,
+            );
         }
+
+        for line in 0..2 {
+            let x = line as f64 * (chord.width + self.stroke_width + self.padding * 2.)
+                + self.stroke_width / 2.;
+            self.draw_line(node, x, 0., x, self.note_ry * 8. + self.stroke_width / 2.);
+        }
+    }
+
+    fn draw_line<T: Node>(&self, node: &mut T, x1: f64, y1: f64, x2: f64, y2: f64) {
+        node.append(
+            Line::new()
+                .set("stroke", "#000")
+                .set("stroke-width", self.stroke_width)
+                .set("x1", x1)
+                .set("y1", y1)
+                .set("x2", x2)
+                .set("y2", y2),
+        )
     }
 }
 
@@ -101,6 +118,8 @@ mod tests {
         let renderer = Renderer {
             note_rx: 10.,
             note_ry: 6.,
+            padding: 10.,
+            stroke_width: 2.,
         };
         renderer.svg(&mut document, Chord::new(&[5, 6, 7], &renderer));
         svg::save("image.svg", &document).unwrap();
