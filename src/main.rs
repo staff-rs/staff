6850d8d649b57828f1480865af8c50fb8176f539
Matt Hunzinger <matthunz2@gmail.com>
2023-09-17 17:16:03 -0400

    Update concoct version

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,16 @@ use staff::{
     Note,
 };
 
-enum Instrument {
-    Piano { keys: MidiSet },
-    Strings,
-}
-
 struct State {
-    instrument: Instrument,
+    keys: MidiSet,
 }
 
 enum Event {
-    Instrument(Instrument),
     Key(MidiNote, bool),
 }
 
 fn piano(midi_set: MidiSet) -> impl View<Web<Event>> {
-    let keys = (0..12)
+    let keys = (0..29)
         .into_iter()
         .map(|x| {
             let midi = MidiNote::from_byte(x);
@@ -37,65 +31,40 @@ fn piano(midi_set: MidiSet) -> impl View<Web<Event>> {
                 class_name.push_str(" selected");
             }
 
-            let html = Html::li(
-                (
-                    class(class_name),
-                    on("click", move |_| Event::Key(midi, !contains_midi)),
-                ),
-                (),
-            );
+            let html = Html::li()
+                .class(class_name)
+                .on("click", move |_| Event::Key(midi, !contains_midi));
             (midi, html)
         })
         .collect::<Vec<_>>();
 
-    Html::ul(class("keys"), keys)
+    Html::ul().class("keys").view(keys)
 }
 
-fn counter(state: &State) -> impl View<Web<Event>> {
+fn app(state: &State) -> impl View<Web<Event>> {
+    let chords = chords(state.keys.clone().into_iter().collect::<Vec<_>>())
+        .map(|chord| ((), Html::li().view(chord.to_string())))
+        .collect::<Vec<_>>();
     (
-        if let Instrument::Piano { ref keys } = state.instrument {
-            let chords = chords(keys.clone().into_iter().collect::<Vec<_>>())
-                .map(|chord| ((), Html::li((), chord.to_string())))
-                .collect::<Vec<_>>();
-            Some((Html::ul((), chords), piano(keys.clone())))
-        } else {
-            None
-        },
-        once(Html::button(
-            on("click", |_| {
-                Event::Instrument(Instrument::Piano {
-                    keys: MidiSet::default(),
-                })
-            }),
-            "Piano",
-        )),
-        once(Html::button(
-            on("click", |_| Event::Instrument(Instrument::Strings)),
-            "Strings",
-        )),
+        Html::ul().class("chords").view(chords),
+        piano(state.keys.clone()),
     )
 }
 
 fn main() {
     concoct::web::run(
         State {
-            instrument: Instrument::Piano {
-                keys: MidiSet::default(),
-            },
+            keys: MidiSet::default(),
         },
         |state, event| match event {
-            Event::Instrument(instrument) => state.instrument = instrument,
-            Event::Key(midi, is_set) => match state.instrument {
-                Instrument::Piano { ref mut keys } => {
-                    if is_set {
-                        keys.push(midi);
-                    } else {
-                        keys.remove(midi);
-                    }
+            Event::Key(midi, is_set) => {
+                if is_set {
+                    state.keys.push(midi);
+                } else {
+                    state.keys.remove(midi);
                 }
-                _ => todo!(),
-            },
+            }
         },
-        counter,
+        app,
     );
 }
